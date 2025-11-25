64ec4e485fb739cb7c5bb21d5f8b56f12feacfe4
c0a24101 <c0a24101a4@edu.teu.ac.jp>
2025-11-25 13:52:59 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,6 +16,12 @@ def main():
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
+    DELTA={
+        pg.K_UP:(0,-5),
+        pg.K_DOWN:(0,5),
+        pg.K_LEFT:(-5,0),
+        pg.K_RIGHT:(5,0)
+        }
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -24,14 +30,10 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0]+=mv[0]
+                sum_mv[1]+=mv[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
